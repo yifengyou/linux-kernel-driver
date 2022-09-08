28578c799f00e38be5f8290f10d7be06bde6b3c8
yifengyou <842056007@qq.com>
2022-09-08 17:35:04 +0800

    fomat 01.helloworld

--- 01.helloworld/src/helloworld.c
+++ 01.helloworld/src/helloworld.c
@@ -8,27 +8,27 @@
 // 在初始化时内核会通过这些函数指针调用这些__init函数指针，
 // 并在整个初始化完成后，释放整个init区段，意味着初始化后丢弃内容，节省空间
 // #define __init  __attribute__ ((__section__ (".init.text")))  __cold
-static int __init simple_init(void)                           //模块入口点没有传入参数（void）
+static int __init helloworld_init(void) //模块入口点没有传入参数（void）
 {
     printk(KERN_INFO "Loading Module\n");
-	printk("%s  %s  %d\n",__FILE__,__func__,__LINE__);
-    return 0;                                   //模块入口点必须返回int，0：成功  !0：失败
+    printk(KERN_INFO "%s  %s  %d\n", __FILE__, __func__, __LINE__);
+    return 0; //模块入口点必须返回int，0：成功  !0：失败
 }
 
 //模块删除时运行此函数
 // __exit 声明表示函数地址放到统一的exit段中
 // #define __exit  __attribute__ (( __section__ (".exit.text"))) __cold
-static void __exit simple_exit(void)                          ////模块离开点没有传入参数（void）
+static void __exit helloworld_exit(void) ////模块离开点没有传入参数（void）
 {
     printk(KERN_INFO "Removing Module\n");
-	printk("%s  %s  %d\n",__FILE__,__func__,__LINE__);
-	// 返回值void，不需要return
+    printk(KERN_INFO "%s  %s  %d\n", __FILE__, __func__, __LINE__);
+    // 返回值void，不需要return
 }
 
 //用于注册模块入口点和出口点的宏
-module_init(simple_init);                       //模块入口点
-module_exit(simple_exit);                       //模块离开点
+module_init(helloworld_init); //模块入口点
+module_exit(helloworld_exit); //模块离开点
 
-MODULE_LICENSE("GPL");                          //模块证书
-MODULE_DESCRIPTION("Simple Module Example");            //模块描述
-MODULE_AUTHOR("nicyou");                           //模块作者
+MODULE_LICENSE("GPL"); //模块证书
+MODULE_DESCRIPTION("Simple Module Example"); //模块描述
+MODULE_AUTHOR("nicyou"); //模块作者
